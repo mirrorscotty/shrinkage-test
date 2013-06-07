11876f3d21c92309818ee808534f40eb3bc000b0
Alex Griessman <alex.griessman@gmail.com>
2013-06-07 15:38:48 -0400

    Random changes that were made randomly

--- poroelastic_analytic/poroelastic_analytic.m
+++ poroelastic_analytic/poroelastic_analytic.m
@@ -51,7 +51,7 @@ classdef poroelastic_analytic
             A = this.alpha(s);
             B = this.beta(s);
             r = this.rho();
-            rf = this.rhof();
+            rf = this.rhof;
             out = E*p^2/R + (r-B*rf)*B/rf+(A-B)^2;
         end
         function [out] = Cq(this, s)
@@ -93,7 +93,7 @@ classdef poroelastic_analytic
             
             out = p/(E*(d1*r2-d2*r1))* ...
                 (d2*(exp(-r1*s*(l-y))-exp(-r1*s*(l+y))) / (s*(1+exp(-2*r1*s*l)))- ...
-                 d1*(exp(-r2*s*(l-y))-exp(-r2*s*(l+y))) / (s*(1+exp(-2*r2*s*l))));
+                d1*(exp(-r2*s*(l-y))-exp(-r2*s*(l+y))) / (s*(1+exp(-2*r2*s*l))));
         end
         
         function [out] = plotu(this, start, fin)
@@ -104,7 +104,51 @@ classdef poroelastic_analytic
             out = u;
             plot(s,u);
         end
+        
+        function [out] = plotut(this, start, fin)
+            [radt, ft] = this.INVLAP(start, end, 500, 6, 20, 19);
+            plot(radt, ft);
+            out = ft;
+        end
+        
+        % INVLAP  Numerical Inversion of Laplace Transforms
+        function [radt,ft]=INVLAP(this, tini,tend,nnt,a,ns,nd);
+            % Fs is formula for F(s) as a string
+            % tini, tend are limits of the solution interval
+            % nnt is total number of time instants
+            % a, ns, nd are parameters of the method
+            % if not given, the method uses implicit values a=6, ns=20, nd=19
+            % it is recommended to preserve a=6
+            % increasing ns and nd leads to lower error
+            % an example of function calling
+            % [t,ft]=INVLAP('s/(s^2+4*pi^2)',0,10,1001);
+            % to plot the graph of results write plot(t,ft), grid on, zoom on
+            
+            if nargin==4
+                a=6; ns=20; nd=19;  end;    % implicit parameters
+            radt=linspace(tini,tend,nnt); % time vector
+            if tini==0  radt=radt(2:1:nnt);  end;  % t=0 is not allowed
+            tic					% measure the CPU time
+            for n=1:ns+1+nd               % prepare necessary coefficients
+                alfa(n)=a+(n-1)*pi*j;
+                beta(n)=-exp(a)*(-1)^n;
+            end;
+            n=1:nd;
+            bdif=fliplr(cumsum(gamma(nd+1)./gamma(nd+2-n)./gamma(n)))./2^nd;
+            beta(ns+2:ns+1+nd)=beta(ns+2:ns+1+nd).*bdif;
+            beta(1)=beta(1)/2;
+            for kt=1:length(radt)                  % cycle for time t
+                tt=radt(kt);
+                s=alfa/tt;                 % complex frequency s
+                bt=beta/tt;
+                for i = 1:length(s)
+                    FF(i) = this.uhaty(s(i), 1);
+                end
+                btF=bt.*FF;          % functional value F(s)
+                ft(kt)=sum(real(btF));     % original f(tt)
+            end;
+            toc
+        end
+        
     end
-    
 end
-
